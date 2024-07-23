f1dd541068151fd06cf638fc6364d16d363ad422
amol <gajare147@gmail.com>
2024-07-23 14:48:35 +0530

    elif program

---
+++ chapter_02/conditional/elif.py
@@ -0,0 +1,21 @@
+#percentage grading
+per= 99
+if per>90 :
+    res='A'
+    
+elif per>80 : 
+    res='B'
+    
+elif per>70 :
+    res='C'
+    
+elif per>60 :
+    res='D'
+    
+elif per>40 :
+    res='Pass'
+    
+else:
+    res='Fail'
+    
+print("your result:",res ,"grade")
