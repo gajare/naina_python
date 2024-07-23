e59b5d5783b705db8ae4ed0423498e44184fe428
amol <gajare147@gmail.com>
2024-07-23 22:27:47 +0530

    break and continue

---
+++ chapter_02/loops/star/star_03.py
@@ -0,0 +1,14 @@
+n=5
+i=1
+while i<=n :
+    j=1
+    while j<=(n-i):
+        print(" ",end= " ")
+        j+=1
+    k=1
+    while k<=(2*i-1):
+
+        print("*",end=" ")
+        k+=1
+    print()
+    i+=1
